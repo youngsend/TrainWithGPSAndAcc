65d00432d340c17f1409c6c3eb7ea1a9feb9d600
youngsend <youngsend@live.com>
2015-06-23 15:39:07 +0800

    Add another two models

--- computeDistanceWithThetaAndFrequency.m
+++ computeDistanceWithThetaAndFrequency.m
@@ -3,6 +3,15 @@ function distance = computeDistanceWithThetaAndFrequency(theta, F)
 FF = F.*F;
 FFF = FF.*F;
 m = size(F, 1);
-V = [ones(m, 1) F FF FFF] * theta;
+if (size(theta, 1) == 4)
+	V = [ones(m, 1) F FF FFF] * theta;
+elseif (size(theta, 1) == 3)
+	V = [ones(m, 1) F FF] * theta;
+else
+	V = [ones(m, 1) F] * theta;
+end
+
 T = 1./F;
 distance = V.*T;
+
+end
